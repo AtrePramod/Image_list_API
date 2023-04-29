d8f5723374d2db8c6a6bc9b59c097b978cae552a
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-04-29 17:39:16 +0530

    179 done add key attributes

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ImageList = (props) => {
 
     const images = props.images.map((image) => {
-        return <img src={image.webformatURL} alt='images' />
+        return <img key={image.id} src={image.webformatURL} alt='images' />
     })
     return (
         <div>
