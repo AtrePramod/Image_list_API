158d2292bf243076b0c892811b374f2f490d3b17
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-04-29 17:09:52 +0530

    177 done use arrow fun with async wait

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import SearchInput from './searchInput';
 import axios from 'axios';
 
 class App extends React.Component {
-    async onSearchSubmit(entry) {
+    state = { images: [] }
+    onSearchSubmit = async (entry) => {
 
         const response = await axios.post(`https://pixabay.com/api/?key=35878181-1910cdbd5f8fdb050ca1027f1&q=${entry}&image_type=photo`)
         console.log(response.data.hits)
+        this.setState({ images: response.data.hits })
     }
     render() {
 
         return (
             <div className='ui container' style={{ marginTop: '30px' }}>
                 <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                we have {this.state.images.length} images
             </div >
         )
     }
