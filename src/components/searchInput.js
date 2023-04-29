d85f9ec4ba5d7613b2f653c307ae487c023f604f
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-04-29 10:11:53 +0530

    171 done

--- src/components/searchInput.js
+++ src/components/searchInput.js
@@ -1,19 +1,24 @@
 import React from 'react'
 
 class SearchInput extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value)
+
+    state = { entry: '' }
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state.entry)
     }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form' action='/'>
+                <form onSubmit={this.onFormSubmit} className='ui form' action='/'>
                     <div className='field'>
                         <div className='ui massive icon input'>
                             <input
                                 type='text'
                                 placeholder='Search..'
-                                onChange={this.onInputChange}
+                                onChange={(event) => this.setState({ entry: event.target.value })}
+                                value={this.state.entry}
                             />
                             <i className='search icon'></i>
                         </div>
